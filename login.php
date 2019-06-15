d01469aff57a2b5e86e0b643c5e83c3fd9d4f2e5
Krishna Teja <packetcode@gmail.com>
2019-06-15 15:42:51 +0530

    some changes

--- login.php
+++ login.php
@@ -7,6 +7,7 @@
             <div class="container">
                 <div class="border border-dark p-3 mt-3 mb-3">
                     <h1> Login</h1>
+                    <?php require "modules/loginform.php"; ?>
                 </div>
             </div>
         </main>
